61dd9100f9b010153f4eed46337090712b0101a3
nameya <nameya89@gmail.com>
2021-07-22 11:48:55 +0000

    Moved the scripts to a directory

---
+++ scripts/sftp-expect.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Enter the Source SFTP Server [server1.example.com] path"
+read path_origin
+
+echo "Enter the Destination SFTP Server [server2.example.com] path"
+read path_destin
+
+
+/usr/bin/expect <<-EOF
+
+spawn scp -r user1@server1.example.com:$path_origin user1@server2.example.com:$path_destin
+
+
+
+expect "password:"
+send "password\r"
+expect "password:"
+send "password\r"
+
+expect eof
+
+catch wait result
+exit [lindex \$result 3]
+
+EOF
+
+status=$?
+
+if test $status -eq 0
+then
+	echo "File $path_origin successfully transferred to Destination SFTP Server --> server2.example.com:$path_destin."
+else
+	echo "File transfer failed."
+fi
+
---
+++ scripts/sftp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Enter the Source SFTP Server"
+read source_server
+
+echo "Enter the Source User Name"
+read  source_user
+
+echo "Enter the Destination SFTP Server"
+read destin_server
+
+echo "Enter the Destination User Name"
+read  destin_user
+
+echo "Enter the Source SFTP Server Path"
+read dir_origin
+
+echo "Enter the Destination SFTP Server Path"
+read dir_destin
+
+scp -r $source_user@$source_server:$dir_origin $destin_user@$destin_server:$dir_destin
+
